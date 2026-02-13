0d1395d8b10c6a7d6f2ee4ee804834751cd3bc5a
mikai <mikailkulik07@gmail.com>
2026-02-13 21:42:40 +0300

    Next js ui initialization

---
+++ ui/admin-ui/app/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen bg-stone-950 font-mono text-white">
+      <div className="flex flex-col w-full">
+        <div className="group border border-black w-full h-20">
+            <div className="flex items-center justify-center w-full">
+
+            </div>
+      </div>
+          <div className={"flex flex-row w-full"}>
+              <div className={"flex w-1/2 border border-white"}>
+                  <div className="flex flex-col gap-5 items-center justify-center w-full">
+                      <div className="container border border-white rounded-lg">
+
+                      </div>
+                  </div>
+              </div>
+              <div className={"flex w-1/2 border border-white"}>
+
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+}
